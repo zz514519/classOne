03b9ba7cf3b8b02001036bf8b87c520be1bd0087
zz514519 <1475430017@qq.com>
2019-10-28 00:35:24 +0800

    class

---
+++ Test2.java
@@ -0,0 +1,12 @@
+/**
+ * Author : Zhang Zhe
+ */
+public class Test2 {
+    public static void main(String[] args) {
+        Test test = new Test();
+        test.setName("张哲");
+        test.setAge(21);
+        test.setSex("man");
+        test.show();
+    }
+}
